5361c6202bd3d7455980480341300ca1f0ed7285
Yash Pandey <ypandey777@yahoo.in>
2025-02-24 00:52:11 +0530

    retries

--- playwright.config.js
+++ playwright.config.js
@@ -20,6 +20,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  // retries:2,     // Retry tests up to 2 times 
+  // --retries=3, // Retry tests up to 2 times from cmd line
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
