ff63c45166efc15df1843c95a5a6b9b84e51a9d5
Yash Pandey <ypandey777@yahoo.in>
2025-02-25 23:31:42 +0530

    Handle Dropdown In Playwright And Verify Dropdown Values

--- tests/dropDown.spec.js
+++ tests/dropDown.spec.js
@@ -1,11 +1,18 @@
-const {test , expect} = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test('selecting an option from a dropdown', async ({page}) => {
-  await page.goto('https://freelance-learn-automation.vercel.app/signup');
-  await page.locator("#state").selectOption({label: 'Karnataka'});
-  await page.waitForTimeout(3000);
-  await page.locator("#state").selectOption({value: 'Delhi'});
-  await page.waitForTimeout(3000);
-  await page.locator("#state").selectOption({index: 2});
-    await page.waitForTimeout(3000);
+test("selecting an option from a dropdown", async ({ page }) => {
+  await page.goto("https://freelance-learn-automation.vercel.app/signup");
+  await page.locator("#state").selectOption({ label: "Karnataka" });
+//   await page.waitForTimeout(3000);
+  await page.locator("#state").selectOption({ value: "Delhi" });
+//   await page.waitForTimeout(3000);
+  await page.locator("#state").selectOption({ index: 2 });
+//   await page.waitForTimeout(3000);
+
+  const allStateValues = await page.locator("#state").textContent();    // get all the values of the dropdown
+    console.log(allStateValues);
+    await expect(allStateValues.includes("Kerala")).toBeTruthy();     // check if the value is present in the dropdown
+//  await expect(allStateValues).toContain("Delhi").toBeTruely();     // check if the value is present in the dropdown
+//     await expect(allStateValues).toContain("Karnataka").toBeTruely();     // check if the value is present in the dropdown
+  
 });
