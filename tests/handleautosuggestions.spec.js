6cdd4a529186fb01a9deca1f4e4c4c6bee98e6ad
Yash Pandey <ypandey777@yahoo.in>
2025-03-01 19:53:20 +0530

    How to Handle Autocomplete in Playwright | Handle Autosuggestion In Playwright

---
+++ tests/handleautosuggestions.spec.js
@@ -0,0 +1,40 @@
+const {test, expect} = require('@playwright/test');
+
+test('Handle autosuggestions using keyboard' , async ({page}) => {
+
+    await page.goto("https://google.com/");
+
+    await page.locator("textarea[name='q']").type("playwright", {delay: 100})
+
+    await page.waitForSelector("//li[@role='presentation']", {state: 'attached'})
+
+    await page.keyboard.press("ArrowDown")
+    await page.keyboard.press("ArrowDown")
+    await page.keyboard.press("Enter")
+    await page.waitForTimeout(3000)
+
+})
+
+test.only('Handle autosuggestions using Loops' , async ({page}) => {
+
+    await page.goto("https://google.com/");
+ 
+    await page.locator("textarea[name='q']").type("playwright", {delay: 100})
+
+    await page.waitForSelector("//li[@role='presentation']")
+
+    const element = await page.$$("//li[@role='presentation']")
+
+    for(let i=0;i<element.length;i++){
+        // if(element[i].textContent() == 'playwright testing'){
+
+        const text = await element[i].textContent();
+        if(text.includes('automation'))
+        {
+            await element[i].click()
+            break;
+        }
+        await page.waitForTimeout(1000)
+    }
+    await page.waitForTimeout(3000)
+})
