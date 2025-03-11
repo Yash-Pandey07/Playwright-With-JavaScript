883ccc76bfa986d07a447b06626e3e87aa8966c5
Yash Pandey <ypandey777@yahoo.in>
2025-03-11 23:22:40 +0530

    Working with multiple windows

---
+++ tests/handle-window.spec.js
@@ -0,0 +1,21 @@
+import {test, expect} from '@playwright/test';
+
+test('Working with multiple windows' , async ({browser}) => {
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://freelance-learn-automation.vercel.app/login");
+
+    const [newPage] = await Promise.all([
+        
+        context.waitForEvent('page'),
+        page.locator("(//a[contains(@href,'facebook')])[1]").click()
+
+    ])
+
+    await newPage.waitForLoadState();
+    await newPage.locator("((//input[@id='«r2»'])[1]").fill("default@email.com");
+    await newPage.waitForTimeout(3000);
+
+
+})
