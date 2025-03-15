b86d2fabdf53d1380a1f10510153a780abb0e853
Yash Pandey <ypandey777@yahoo.in>
2025-03-16 02:26:13 +0530

    How To Handle Dynamic Network Call In Playwright

---
+++ tests/loginWithJSONfiledata.spec.js
@@ -0,0 +1,39 @@
+const {test , expect} = require('@playwright/test');
+
+const data = JSON.parse(JSON.stringify(require("../testdata.json")));
+
+
+// test('Login with JSON file data', async ({page}) => {
+
+//     await page.goto("https://freelance-learn-automation.vercel.app/login");
+
+//     // const data = require('../testdata.json');
+//     await page.getByPlaceholder("Enter Email").type(data.username);
+//     await page.getByPlaceholder("Enter Password").type(data.password);
+//     // await page.getByRole("button",{name:"Sign in"}).click();
+//     // await page.waitForTimeout(3000);
+
+//     // await expect(page).toHaveURL(/dashboard/);
+
+//     // await page.locator("//span[text()='Manage']").hover();
+//     // await page.locator("//a[normalize-space()='Manage Courses']").click();
+//     // await page.waitForTimeout(3000);
+
+//     // await page.getByText('Logout').click();
+//     // await expect(page).toHaveURL(/login/);
+// });
+
+test.describe('Login with 3 users data with JSON file', () => {
+    for(const user of data){
+        test.describe(`Login with ${user.username} data`, () => {
+            
+            test('Login to Application', async ({page}) => {
+
+                await page.goto("https://freelance-learn-automation.vercel.app/login");
+                await page.getByPlaceholder("Enter Email").type(user.username);
+                await page.getByPlaceholder("Enter Password").type(user.password);
+                await page.waitForTimeout(3000);
+            });
+        });
+    }
+});
--- tests/waitForLoadState.spec.js
+++ tests/waitForLoadState.spec.js
@@ -3,11 +3,14 @@ const {test, expect} = require('playwright/test');
 test('Wait for Load State', async ({page}) => {
 
     await page.goto('https://freelance-learn-automation.vercel.app/login'); 
-    await page.getByName('New user? Signup').click();
+    await page.getByText('New user? Signup').click();
     
+    //let network load
+    await page.waitForLoadState("networkidle");
+
     const count = await page.locator("//input[@type = 'checkbox']").count();
 
     expect(count).toBe(6);
 
 
-});
+}); 
